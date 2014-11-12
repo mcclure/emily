43a04384da95c0b0a8a086a595d9aecd1bb9011c
mcc <andi.m.mcclure@gmail.com>
2014-11-12 09:05:19 -0800

    Comment Token and about half of Tokenize

--- src/token.ml
+++ src/token.ml
@@ -1,32 +1,42 @@
+(* Records the original source position of a token *)
 type codePosition = {
     fileName : string option;
     lineNumber : int;
     lineOffset : int;
 }
 
+(* Make codePosition.fileName human-readable *)
 let fileNameString n = (match n with None -> "<Input>" | Some s -> s)
 
+(* Make codePosition human-readable *)
 let positionString p = Printf.sprintf "[%s line %d ch %d]"
     (fileNameString p.fileName) p.lineNumber p.lineOffset
 
+(* What are the rules for descending into this group? *)
 type tokenGroupKind =
     | Plain                        (* Parenthesis *)
     | Scoped                       (* Create a new scope within this group *)
     | Box                          (* Create a new object *)
 
+(* Is this group a closure? What kind? *)
 type tokenClosureKind =
     | NonClosure                   (* Is not a function *) 
     | Closure                      (* No-argument function-- should appear post-macro only *)
     | ClosureWithBinding of string (* Function with argument-- should appear post-macro only *)
 
-type codeSequence = token list list (* A codeSequence is a list of lines. A line is a list of tokens. *)
+(* Representation of a tokenized code blob. *)
+(* A codeSequence is a list of lines. A line is a list of tokens. *)
+(* A token may be a group with its own codeSequence. *)
+type codeSequence = token list list
 
+(* Data content of a group-type token *)
 and tokenGroup = {
     kind : tokenGroupKind;      (* Group kind *)
     closure : tokenClosureKind;  (* Closure kind, if any *)
     items : codeSequence; (* Group is a list of lines, lines are a list of tokens *)
 }
 
+(* Data content of a token *)
 and tokenContents = 
     | Word of string   (* Alphanum *)
 (*  | Symbol of string    Punctuation-- appears pre-macro only. Disabled until macros are back *)
@@ -35,21 +45,27 @@ and tokenContents =
     | Number of float
     | Group of tokenGroup
 
+(* A token. Effectively, an AST node. *)
 and token = {
     at : codePosition;
     contents : tokenContents;
 }
 
+(* Quick constructor for token *)
 let makeToken position contents = {
     at = position;
     contents = contents;
 }
 
+(* Quick constructor for token, group type *)
 let makeGroup position closure kind items = 
     makeToken position ( Group { kind=kind; closure=closure; items=items; } )
 
+(* TODO: Put these in their own file? *)
+(* "Disassemble" a token tree into a human-readable string (specializable) *)
 let rec dumpTree groupPrinter token =
     match token.contents with
+    (* For a simple (nongrouping) token, return a string for just the item *)
     | Word x (* | Symbol x *) -> x
     | String x -> "\"" ^ x ^ "\""
     | Atom x -> "." ^ x
@@ -63,8 +79,11 @@ let rec dumpTree groupPrinter token =
             | NonClosure -> ""
             | Closure -> "^"
             | ClosureWithBinding binding -> "^" ^ binding) ^ l
+        (* GroupPrinter is an argument function which takes the left group symbol, right group
+           symbol, and group contents, and decides how to format them all. *)
         in groupPrinter token l r items
 
+(* "Disassemble" a token tree into a human-readable string (specialized for looking like code) *)
 let dumpTreeTerse token =
     let rec groupPrinter token l r items =
         l ^ ( String.concat "; " (
@@ -73,6 +92,7 @@ let dumpTreeTerse token =
         ) ) ^ r
     in dumpTree groupPrinter token
 
+(* "Disassemble" a token tree into a human-readable string (specialized to show token positions) *)
 let dumpTreeDense token =
     let rec oneToken x = Printf.sprintf "%s %s" (positionString x.at) (dumpTree groupPrinter x)
     and groupPrinter token l r items =
--- src/tokenize.ml
+++ src/tokenize.ml
@@ -1,42 +1,72 @@
-let digit = [%sedlex.regexp? '0'..'9']
-let number = [%sedlex.regexp? Plus digit]
+(* This is Emily's "parser", which I'm calling a tokenizer since it doesn't do much. 
+   It scans left to right, categorizing tokens, pushing on left parens and popping on right.
+   The parts we'd think of a "parser" as usually doing will be handled in a second,
+   currently unimplemented, macro-processing step. *)
 
-let dequote s =
-    let len = String.length s in String.sub s 1 (len-2)
-
+(* Tokenize uses sedlex which is inherently stateful, so tokenize for a single source string is stateful. 
+   Here's the basic state for the file parse-- basically just recording the position of the last seen newline. *)
 type tokenize_state = {
     mutable lineStart: int;
     mutable line: int
 }
 
+(* Entry point to tokenize, takes a filename and a lexbuf *)
 let rec tokenize name buf : Token.token = 
-    let state = {lineStart=0; line=1} in
-    let stateNewline () = state.lineStart <- Sedlexing.lexeme_end buf; state.line <- state.line + 1 in 
-    let currentPosition () = Token.{fileName=name; lineNumber=state.line; lineOffset = Sedlexing.lexeme_end buf-state.lineStart} in
-    let currentPositionString () = Token.positionString(currentPosition()) in
+    (* -- Helper regexps -- *)
+    let digit = [%sedlex.regexp? '0'..'9'] in
+    let number = [%sedlex.regexp? Plus digit] in
     let letterPattern = [%sedlex.regexp? 'a'..'z'|'A'..'Z'] in (* TODO: should be "alphabetic" *)
     let wordPattern = [%sedlex.regexp? letterPattern, Star ('A'..'Z' | 'a'..'z' | digit) ] in
     let floatPattern = [%sedlex.regexp? '.',number | number, Opt('.', number) ] in
-    let parseFail mesg = failwith(Printf.sprintf "%s %s" mesg (currentPositionString())) in
+
+    (* Helper function: We treat a list as a stack by appending elements to the beginning,
+       but this means we have to do a reverse operation to seal the stack at the end. *)
     let cleanup = List.rev in
+
+    (* -- State management machinery -- *)
+    (* Current tokenizer state *)
+    let state = {lineStart=0; line=1} in
+    (* Call when the current selected sedlex match is a newline. Mutates tokenizer state. *)
+    let stateNewline () = state.lineStart <- Sedlexing.lexeme_end buf; state.line <- state.line + 1 in 
+    (* Use tokenizer state to translate sedlex position into a codePosition *)
+    let currentPosition () = Token.{fileName=name; lineNumber=state.line; lineOffset = Sedlexing.lexeme_end buf-state.lineStart} in
+    (* Use tokenizer state to translate sedlex position into a human-readable string. *)
+    let currentPositionString () = Token.positionString(currentPosition()) in
+    (* Parse failure. Append human-readable code position string. *)
+    let parseFail mesg = failwith(Printf.sprintf "%s %s" mesg (currentPositionString())) in
+    
+    (* -- Parsers -- *)
+
+    (* Sub-parser: double-quoted strings. Call after seeing opening quote mark *)
     let rec quotedString () = 
+        (* This parser works by statefully adding chars to a string buffer *)
         let accum = Buffer.create 1 in
+        (* Helper function adds a literal string to the buffer *)
         let add = Buffer.add_string accum in
+        (* Helper function adds a sedlex match to the buffer *)
         let addBuf() = add (Sedlexing.Utf8.lexeme buf) in
+        (* Operate *)
         let rec proceed () =
+            (* Call after seeing a backslash. Matches one character, returns string escape corresponds to. *)
             let escapedChar () = 
                 match%sedlex buf with
                     | '\\' -> "\\"
                     | '"' -> "\""
                     | 'n'  -> "\n"
                     | _ -> parseFail "Unrecognized escape sequence" (* TODO: devour newlines *)
+            (* Chew through until quoted string ends *)
             in match%sedlex buf with
+                (* Treat a newline like any other char, but since sedlex doesn't track lines, we have to record it *)
                 | '\n' -> stateNewline(); addBuf(); proceed()
+                (* Backslash found, trigger escape handler *)
                 | '\\' -> add (escapedChar()); proceed()
+                (* Unescaped quote found, we are done. Return the data from the buffer. *)
                 | '"'  -> Buffer.contents accum
+                (* Any normal character add to the buffer and proceed. *)
                 | any  -> add (Sedlexing.Utf8.lexeme buf); proceed()
                 | _ -> parseFail "Unrecognized escape sequence"
         in proceed()
+
     in let rec proceed (groupSeed : Token.token list list -> Token.token) lines line =
         let localToken = Token.makeToken (currentPosition()) in
         let closePattern = [%sedlex.regexp? '}' | ')' | ']' | eof] in
