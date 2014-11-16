14c4034fb5e2b438cf857ad38c7b32078c13bbc7
mcc <andi.m.mcclure@gmail.com>
2014-11-16 11:44:44 -0800

    ./package/emily -e 'println 3' works

--- src/main.ml
+++ src/main.ml
@@ -3,6 +3,7 @@ let () =
 	    let a = match target with
 	    	| Options.File f -> Tokenize.tokenize_channel (open_in f)
 	    	| Options.Stdin -> Tokenize.tokenize_channel stdin
+	    	| Options.Literal s -> Tokenize.tokenize_string s
 		in
 	    (* print_endline (Token.dumpTreeDense a) *)
 	    Execute.execute a
--- src/options.ml
+++ src/options.ml
@@ -1,7 +1,7 @@
 let version = "0.0 DEVELOPMENT"
 let machineVersion = [0,0,0]
 
-type executionTarget = Stdin | File of string
+type executionTarget = Stdin | File of string | Literal of string
 
 type optionSpec = {
     mutable targets : executionTarget list
@@ -19,14 +19,19 @@ let () =
         ("-", Arg.Unit(fun () -> (* Arg's parser means the magic - argument must be passed in this way. *)
             if !seenStdin then failwith "Attempted to parse stdin twice; that doesn't make sense?"
                 else seenStdin := true; targets := Stdin :: !targets
-        ), "") (* No summary, this shouldn't be listed with options. *)
+        ), ""); (* No summary, this shouldn't be listed with options. *)
+
+        ("-e", Arg.String(fun f ->
+                targets := Literal f :: !targets
+        ), "Execute code inline") (* No summary, this shouldn't be listed with options. *)
     ] targetParse
 
 ("Emily language interpreter: version " ^ version ^ {|
 
 Sample usage:
-    emily filename.em # Execute program
-    emily -           # Execute from stdin
+    emily filename.em    # Execute program
+    emily -              # Execute from stdin
+    emily -e "println 3" # Execute from command line
 Options:|})
 
     ; run.targets <- List.rev !targets
