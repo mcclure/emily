8b175ad70bba579f3a6179a9849075fa35da5036
mcc <andi.m.mcclure@gmail.com>
2014-11-16 10:21:50 -0800

    Cmdline improvements. These work ./package/emily - < sample/ski.em ./package/emily sample/ski.em

--- src/options.ml
+++ src/options.ml
@@ -12,23 +12,21 @@ let run = {targets=[]}
 let () = 
     let targets = ref [] in
     let seenStdin = ref false in
+    
+    let targetParse t = targets := File t :: !targets in
 
-    let targetParse t =
-        let v = match t with
-            | "<>" ->
-                if !seenStdin then failwith "Attempted to parse stdin twice; that doesn't make sense?"
-                else seenStdin := true; Stdin
-            | t -> File t in
-        targets := v :: !targets
-    in
-
-    Arg.parse [] targetParse
+    Arg.parse [
+        ("-", Arg.Unit(fun () -> (* Arg's parser means the magic - argument must be passed in this way. *)
+            if !seenStdin then failwith "Attempted to parse stdin twice; that doesn't make sense?"
+                else seenStdin := true; targets := Stdin :: !targets
+        ), "") (* No summary, this shouldn't be listed with options. *)
+    ] targetParse
 
 ("Emily language interpreter: version " ^ version ^ {|
 
 Sample usage:
     emily filename.em # Execute program
-    emily "<>"          # Execute from stdin
+    emily -           # Execute from stdin
 Options:|})
 
     ; run.targets <- List.rev !targets
