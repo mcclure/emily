2073500864939b92c12ddb059bce7f1b2aca4dc3
Erik Osheim <d_m@plastic-idolatry.com>
2015-01-21 01:26:05 -0500

    Some gestures towards usability.
    The REPL starts with a banner, produces a prompt (>>>),
    and has an incredibly rudimentary system of meta commands.
    
    Right now, %help and %quit are grabbed by the REPL, to
    produce help output or quit, respectively. Eventually, my
    goal is to support other more interesting commands, to
    inspect the scope, look up names, do search/completion,
    and so on.
    
    There's an argument to be made that maybe % commands
    should be implemented in Emily. I think this is a good
    idea but right now it's not clear most of the commands
    will have good implementations. I guess it depends on
    how what kind of introspection Emily offers (something
    I don't know a lot about yet).

--- src/repl.ml
+++ src/repl.ml
@@ -1,15 +1,49 @@
-(* Return true if string starts with a tab or space, false otherwise. *)
+(* Shows a friendly banner *)
+let doBanner () =
+  print_endline("emily v0.1 repl");
+  print_endline("%help for help, %quit to quit");
+  print_endline("have fun!")
+
+(* Shows a help message *)
+let doHelp () =
+  print_endline("put a helpful message here")
+
+(* Shows a help message *)
+let doUnknownCmd cmd =
+  print_string "ERROR: unknown command ";
+  print_endline cmd
+
+(* Check if the string 's' starts with 'prefix'. *)
+let startsWith s prefix =
+  let n = String.length prefix in
+  let rec cmp i =
+    if i == n then true
+    else if (String.get s i) != (String.get prefix i) then false
+    else cmp (i + 1) in
+  (String.length s) >= n && cmp 0
+
+(* Check if the string 's' ends with 'suffix'. *)
+let endsWith s suffix =
+  let m = String.length s in
+  let n = String.length suffix in
+  let rec cmp i j =
+    if j == n then true
+    else if (String.get s i) != (String.get suffix j) then false
+    else cmp (i + 1) (j + 1) in
+  m >= n && cmp (m - n) 0
+
+(* Check if the string 's' starts with a tab or space. *)
 let isIndented s =
   if (String.length s) == 0 then false
-  else match (String.get s 0) with
-       | '\t' -> true
-       | ' ' -> true
-       | _ -> false
+  else let c = (String.get s 0) in c == '\t' || c == ' '
 
-(* Return true if string starts with a backslash, false otherwise. *)
+(* Check if the string 's' ends with a backslash. *)
 let isContinued s =
-  let len = String.length s in
-  if len == 0 then false else (String.get s (len - 1)) == '\\'
+  endsWith s "\\"
+
+(* Check if the string 's' starts with %. *)
+let isMetaCommand s =
+  startsWith s "%"
 
 (* Runs the REPL.
 
@@ -34,8 +68,10 @@ let repl targets =
   let line = ref "" in
   let lines = ref [] in
 
-  (* read a line of user input *)
-  let readInputLine =
+  (* display a prompt, then read a line from the user *)
+  let promptAndReadLine s =
+    print_string s;
+    flush stdout;
     line := input_line stdin;
     lines := !line :: !lines in
 
@@ -57,28 +93,49 @@ let repl targets =
       | Options.File f -> runFile f
       | _ -> () in
 
-  (* run all lines of user input *)
-  let runInput lines =
-    let data = String.concat "\n" (List.rev !lines) in
+  (* run the given string *)
+  let runString data =
     runCode (Tokenize.tokenize_string Token.Cmdline data) in
 
-  (* first, run any files provided as arguments *)
-  List.iter runTargetFiles targets;
+  (* run all lines of user input *)
+  let runInput () =
+    runString (String.concat "\n" (List.rev !lines)) in
+
+  let runUserFiles () =
+    List.iter runTargetFiles targets in
+
+  (* the user entered a meta command -- handle it! *)
+  let handleMetaCommand cmd =
+    match cmd with
+    | "%quit" -> raise End_of_file
+    | "%help" -> doHelp ()
+    | _ -> doUnknownCmd cmd in
+
+  (* the user is entering code -- read it all and then run it *)
+  let handleCode () =
+    (* keep reading lines if continued with \ *)
+    while (isContinued !line) do promptAndReadLine "..> " done;
+
+    (* now run the accumulated lines *)
+    runInput ();
+
+    (* flush stdout so any output is immediately visible *)
+    flush stdout in
+
+  doBanner ();
+
+  (* next, run any files provided as arguments *)
+  runUserFiles ();
 
   try
     (* as long as the user hasn't sent EOF (Control-D), read input *)
     while true do
-      (* read one line of input *)
-      readInputLine;
 
-      (* as long as the line is continued, keep accumulating input *)
-      while (isContinued !line) do readInputLine done;
+      (* draw a prompt, and read one line of input *)
+      promptAndReadLine ">>> ";
 
-      (* now run the accumulated lines *)
-      runInput lines;
-
-      (* flush stdout so any output is immediately visible *)
-      flush stdout;
+      (* handle the user's input appropriately *)
+      if (isMetaCommand !line) then handleMetaCommand !line else handleCode ();
 
       (* empty lines, since they have all been executed *)
       lines := [];
